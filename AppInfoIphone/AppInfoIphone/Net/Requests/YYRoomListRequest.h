ec200fc502e64aa036865ce0701574a1501bc5f5
yangyanan <yyn1110@163.com>
2013-08-23 15:46:22 +0800

    net ok

---
+++ AppInfoIphone/AppInfoIphone/Net/Requests/YYRoomListRequest.h
@@ -0,0 +1,13 @@
+//
+//  YYRoomListRequest.h
+//  AppInfoIphone
+//
+//  Created by kuxing on 13-8-23.
+//  Copyright (c) 2013年 kuxing. All rights reserved.
+//
+
+#import "KXBaseFormDataRequest.h"
+
+@interface YYRoomListRequest : KXBaseFormDataRequest
+
+@end
---
+++ AppInfoIphone/AppInfoIphone/Net/Requests/YYRoomListRequest.m
@@ -0,0 +1,13 @@
+//
+//  YYRoomListRequest.m
+//  AppInfoIphone
+//
+//  Created by kuxing on 13-8-23.
+//  Copyright (c) 2013年 kuxing. All rights reserved.
+//
+
+#import "YYRoomListRequest.h"
+
+@implementation YYRoomListRequest
+
+@end
